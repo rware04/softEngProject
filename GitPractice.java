9fbbdd9d7f83a490df5fccb85c40021690cc655b
Robbie Ware <robbie.c.ware@gmail.com>
2022-01-20 11:45:20 +0000

    test practice

---
+++ GitPractice.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testing;
+
+/**
+ * @author robertware
+ *
+ */
+public class GitPractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
